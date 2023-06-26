9b0c05b237d5fa6051197197b750edb3a7abceae
Smed3 <127975399+Smed3@users.noreply.github.com>
2023-06-26 13:00:44 +0300

    Add project files.

---
+++ Words/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Words
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(239, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текст з файлу";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(65, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(557, 288);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(547, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(682, 653);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Робота з текстом";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Words/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Words
+{
+    public partial class Form1 : Form
+    {
+        string[] words;
+        string displayedWord = "";
+        bool isWordDisplayed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog1.FileName;
+                using (StreamReader reader = new StreamReader(selectedFilePath))
+                {
+                    string fileContent = reader.ReadToEnd();
+
+                    textBox1.Text = fileContent;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            words = textBox1.Text.Split(new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            HandleSelectedText(textBox1.SelectedText, textBox1.SelectionStart);
+        }
+
+        private void HandleSelectedText(string selectedText, int selectionStart)
+        {
+            string pattern = "^[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ’-]+$";
+            bool containsNonUkrainianLetters = !Regex.IsMatch(selectedText, pattern, RegexOptions.IgnoreCase);
+            if (containsNonUkrainianLetters || !words.Contains(selectedText))
+            {
+                ClearLetterImages();
+                return;
+            }
+            char[] separators = new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?' };
+            try
+            {
+                if (separators.Contains(textBox1.Text[selectionStart - 1]) && separators.Contains(textBox1.Text[selectionStart + selectedText.Length]))
+                    DisplayLetterImages(selectedText, 10, ClientSize.Width - 10, ClientSize.Height - 150);
+            }
+            catch
+            {
+                if (selectionStart - 1 < 0 && separators.Contains(textBox1.Text[selectionStart + selectedText.Length]))
+                    DisplayLetterImages(selectedText, 10, ClientSize.Width - 10, ClientSize.Height - 150);
+                else if (separators.Contains(textBox1.Text[selectionStart - 1]) && selectionStart + selectedText.Length >= textBox1.Text.Length)
+                    DisplayLetterImages(selectedText, 10, ClientSize.Width - 10, ClientSize.Height - 150);
+            }
+        }
+
+        private void DisplayLetterImages(string word, int x1, int x2, int y)
+        {
+            word = word.ToUpper();
+            if (displayedWord == word && isWordDisplayed)
+                return;
+            else
+                ClearLetterImages();
+            displayedWord = word;
+            int areaWidth = x2 - x1;
+
+            int totalImagesWidth = 0;
+            foreach (char letter in word)
+            {
+                Image image = Image.FromFile(@"..\\..\\Resources\\Letters\\" + letter + ".png");
+                totalImagesWidth += image.Width;
+            }
+
+            if (totalImagesWidth > this.ClientSize.Width)
+                totalImagesWidth = this.ClientSize.Width - 10;
+
+            int startX = x1 + (areaWidth - totalImagesWidth) / 2;
+
+            foreach (char letter in word)
+            {
+                Image image = Image.FromFile(@"..\\..\\Resources\\Letters\\" + letter + ".png");
+                PictureBox pb = new PictureBox();
+                int size = totalImagesWidth / word.Length;
+                pb.Size = new Size(size, size);
+                pb.Location = new Point(startX, y);
+                pb.BackgroundImage = image;
+                pb.BackgroundImageLayout = ImageLayout.Stretch;
+                Controls.Add(pb);
+                startX += size;
+            }
+            isWordDisplayed = true;
+        }
+
+        private void ClearLetterImages()
+        {
+            if (isWordDisplayed == false)
+                return;
+            while (this.Controls.OfType<PictureBox>().Any())
+            {
+                PictureBox pictureBox = this.Controls.OfType<PictureBox>().First();
+                this.Controls.Remove(pictureBox);
+                pictureBox.Dispose();
+            }
+            isWordDisplayed = false;
+        }
+    }
+}
